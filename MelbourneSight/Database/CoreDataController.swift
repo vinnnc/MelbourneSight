1ff18c526491a8986c622e08779c8c6ee52a03e8
Wenchu Du <duwenchu999@gmail.com>
2019-08-31 15:04:01 +1000

    Added Database group with CoreDataController and MulticastDelegate

---
+++ MelbourneSight/Database/CoreDataController.swift
@@ -0,0 +1,100 @@
+//
+//  CoreDataController.swift
+//  MelbourneSight
+//
+//  Created by Wenchu Du on 2019/8/31.
+//  Copyright © 2019 Wenchu Du. All rights reserved.
+//
+
+import UIKit
+import CoreData
+
+class CoreDataController: NSObject, NSFetchedResultsControllerDelegate, DatabaseProtocol {
+    var listeners = MulticastDelegate<DatabaseListener>()
+    var persistentContainer: NSPersistentContainer
+    
+    // Results
+    var allSightsFetchedResultsController: NSFetchedResultsController<Sight>?
+    
+    override init() {
+        persistentContainer = NSPersistentContainer(name: "MelbourneSight")
+        persistentContainer.loadPersistentStores() { (description, error) in
+            if let error = error {
+                fatalError("Failed to load Core Data stack: \(error)") }
+        }
+        
+        super.init()
+        
+        // If there are no sight in the database assume that the app is running
+        // for the first time. Initial sights.
+//        if fetchAllHeroes().count == 0 {
+//            createDefaultEntries() }
+    }
+    
+    func saveContext() {
+        if persistentContainer.viewContext.hasChanges {
+            do {
+                try persistentContainer.viewContext.save()
+            } catch {
+                fatalError("Failed to save data to Core Data: \(error)") }
+        }
+    }
+    
+    func addSight(name: String, desc: String, latitude: Double, longitude: Double, mapIcon: String, photo: String) -> Sight {
+        let sight = NSEntityDescription.insertNewObject(forEntityName: "Sight", into:
+            persistentContainer.viewContext) as! Sight
+        sight.name = name
+        sight.desc = desc
+        sight.latitude = latitude
+        sight.longitude = longitude
+        sight.mapIcon = mapIcon
+        sight.photo = photo
+        
+        saveContext()
+        return sight
+    }
+    
+    func deleteSight(sight: Sight) {
+        persistentContainer.viewContext.delete(sight)
+        saveContext()
+    }
+    
+    func addListener(listener: DatabaseListener) {
+        listeners.addDelegate(listener)
+        listener.onSightsChange(change: .update, sights: fetchAllSights())
+    }
+    
+    func removeListener(listener: DatabaseListener) {
+        listeners.removeDelegate(listener)
+    }
+    
+    func fetchAllSights() -> [Sight] {
+        if allSightsFetchedResultsController == nil {
+            let fetchRequest: NSFetchRequest<Sight> = Sight.fetchRequest()
+            let nameSortDescriptor = NSSortDescriptor(key: "name", ascending: true)
+            fetchRequest.sortDescriptors = [nameSortDescriptor]
+            allSightsFetchedResultsController = NSFetchedResultsController<Sight>(fetchRequest: fetchRequest, managedObjectContext: persistentContainer.viewContext, sectionNameKeyPath: nil, cacheName: nil)
+            allSightsFetchedResultsController?.delegate = self
+            
+            do {
+                try allSightsFetchedResultsController?.performFetch()
+            } catch {
+                print("Fetch Request failed: \(error)")
+            }
+        }
+        
+        var sights = [Sight]()
+        if allSightsFetchedResultsController?.fetchedObjects != nil {
+            sights = (allSightsFetchedResultsController?.fetchedObjects)!
+        }
+        
+        return sights
+    }
+    
+    // MARK: - Fetched Results Conttroller Delegate
+    func controllerDidChangeContent(_ controller: NSFetchedResultsController<NSFetchRequestResult>) {
+        listeners.invoke {
+            (listener) in listener.onSightsChange(change: .update, sights: fetchAllSights())
+        }
+    }
+}
---
+++ MelbourneSight/Database/MulticastDelegate.swift
@@ -0,0 +1,49 @@
+//
+//  MulticastDelegate.swift
+//  MelbourneSight
+//
+//  Created by Wenchu Du on 2019/8/31.
+//  Copyright © 2019 Wenchu Du. All rights reserved.
+//
+
+import Foundation
+
+class MulticastDelegate<T> {
+    private var delegates = Set<WeakObjectWrapper>()
+    
+    func addDelegate(_ delegate: T) {
+        let delegateObject = delegate as AnyObject
+        delegates.insert(WeakObjectWrapper(value: delegateObject))
+    }
+    
+    func removeDelegate(_ delegate: T) {
+        let delegateObject = delegate as AnyObject
+        delegates.remove(WeakObjectWrapper(value: delegateObject))
+    }
+    
+    func invoke(invocation: (T) -> ()) {
+        delegates.forEach { (delegateWrapper) in
+            if let delegate = delegateWrapper.value {
+                invocation(delegate as! T)
+            }
+        }
+    }
+}
+
+private class WeakObjectWrapper: Equatable, Hashable {
+    weak var value: AnyObject?
+    
+    init(value: AnyObject) {
+        self.value = value
+    }
+    
+    // Hash based on the address (pointer) of the value.
+    func hash(into hasher: inout Hasher) {
+        hasher.combine(ObjectIdentifier(value!).hashValue)
+    }
+    
+    // Equate based on equality of the value pointers of two wrappers.
+    static func == (lhs: WeakObjectWrapper, rhs: WeakObjectWrapper) -> Bool {
+        return lhs.value === rhs.value
+    }
+}
